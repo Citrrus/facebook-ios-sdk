eefce82fb9f8028c9b88f171fe91909bcef6de81
Christine Abernathy <caabernathy@caabernathymbp.thefacebook.com>
2011-08-12 14:56:59 -0700

    Getting Started iOS App, modelling Matt Kelly's Getting Started Mobile Web app
    Summary:
    Added a sample application that is similar to http://www.mattwkelly.com/mwa/getting-started/.
    Starts off with a splash screen then a log in button. The rest of the flows are similar to
    the web Getting Started app. UI is created programmatically, i.e. no dependency on NIBs.
    
    Before testing be sure to add your app ID to GettingStarted-Info.plist for
    SSO suppport and to GettingStartedAppDelegate.m.
    
    Task ID: 658310
    
    Blame Rev:
    
    Reviewers: lshepard,selekman, yariv
    
    CC: yariv, brent, mattwkelly, achao, security-diffs@lists, vksgupta, manish, lshepard
    
    Test Plan:
    Ran the application from iPhone simulator (4.2), iPhone device (4.3), and iPad Simulator (4.2).
    Tested following functionality:
    - SSO when logged in to Facebook app
    - SSO when logged in to mobile browser (no Facebook app)
    - SSO when app previously ran then logged out of mobile browser
    - Login, Authorization Removal (from app and from website), Logout, Extended Permission request (user_likes)
    - Logging in, not allowing app permissions
    - Logging in, allowing app permissions
    - News Feed (User, Friend)
    - App Requests (To many, to both app and non-app users, to app users, to a single friend)
    - Graph API (basic info, friends, view recent check-ins, nearby places, check-in, upload photo, upload video)
    - Test asking for recent check-ins when extended permission not granted yet
    - Test trying to check-in when extended permission not granted yet
    
    Revert Plan:
    OK
    
    Tags:
    
    - begin *PUBLIC* platform impact section -
    Bugzilla: #
    - end platform impact -
    
    Differential Revision: 303317

---
+++ sample/GettingStarted/GettingStarted/GettingStartedAppDelegate.m
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2010 Facebook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "GettingStartedAppDelegate.h"
+#import "RootViewController.h"
+#import "DataSet.h"
+
+// Your Facebook APP Id must be set before running this example
+// See http://www.facebook.com/developers/createapp.php
+// Also, your application must bind to the fb[app_id]:// URL
+// scheme (substitue [app_id] for your real Facebook app id).
+static NSString* kAppId = nil;
+
+@implementation GettingStartedAppDelegate
+
+
+@synthesize window=_window;
+
+@synthesize navigationController=_navigationController;
+
+@synthesize facebook;
+
+@synthesize apiData;
+
+@synthesize userPermissions;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Initialize Facebook
+    RootViewController *rootViewController = (RootViewController *) self.navigationController.topViewController;
+    facebook = [[Facebook alloc] initWithAppId:kAppId andDelegate:rootViewController];
+    
+    // Initialize API data (for views, etc.)
+    apiData = [[DataSet alloc] init];
+    
+    // Initialize user permissions
+    userPermissions = [[NSMutableDictionary alloc] initWithCapacity:1];
+    
+    // Override point for customization after application launch.
+    // Add the navigation controller's view to the window and display.
+    self.window.rootViewController = self.navigationController;
+    [self.window makeKeyAndVisible];
+    
+    // Check App ID:
+    // This is really a warning for the developer, this should not
+    // happen in a completed app
+    if (!kAppId) {
+        UIAlertView *alertView = [[UIAlertView alloc] 
+                                  initWithTitle:@"Setup Error" 
+                                  message:@"Missing app ID. You cannot run the app until you provide this in the code." 
+                                  delegate:self 
+                                  cancelButtonTitle:@"OK" 
+                                  otherButtonTitles:nil, 
+                                  nil];
+        [alertView show];
+        [alertView release];
+    }
+    
+    return YES;
+}
+
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+    return [self.facebook handleOpenURL:url];
+}
+
+- (void)dealloc
+{
+    [_window release];
+    [_navigationController release];
+    [facebook release];
+    [apiData release];
+    [userPermissions release];
+    [super dealloc];
+}
+
+#pragma mark - UIAlertViewDelegate methods
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    // Quit the app
+    exit(1);
+}
+
+@end
